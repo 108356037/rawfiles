01b234e0d3256d7480affbf8213148c8cb11a11d
david_tsai <108356037@nccu.edu.tw>
2022-11-21 13:09:27 +0800

    add key creation & encryption

--- setup-tink.sh
+++ setup-tink.sh
@@ -18,4 +18,19 @@ bazel build tinkey
 export PATH=$PATH:$HOME/tink/tools/bazel-bin/tinkey
 
 cd $HOME
-tinkey list-key-templates
+
+# create a AES256_GCM key using tink
+tinkey create-keyset \
+       --out test_use_service_key \
+       --out-format binary \
+       --key-template AES256_GCM_RAW
+
+# encrypt key with openssl and public key from GCP
+openssl pkeyutl \
+        -encrypt \
+        -in ./test_use_service_key \
+        -pubin -inkey ./rawfiles/test-use-pubkey.pub \
+        -pkeyopt rsa_padding_mode:oaep \
+        -pkeyopt rsa_oaep_md:sha256 \
+        -pkeyopt rsa_mgf1_md:sha256 \
+        -out ./encrypted_test_use_service_key
